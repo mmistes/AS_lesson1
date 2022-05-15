a7dd160dd3693f60ce023b9eceead5f7f92ba929
mmistes <93210581+mmistes@users.noreply.github.com>
2022-05-15 23:20:34 +0300

    Версия 3.0 Добавлено: 1) новые фоновые рисунки для светлой и тёмной темы. 2) кнопки сборса и удаления последнего символа 3) функционал калькулятора, теперь он работает
    Реализовано:
    1) горизонтальный интерфейс для светлой и тёмной темы.
    2) функционал калькулятора с 0 в начале и одной точкой в числе.
    
    Удалено:
    1) старое фоновое изобрашение.
    2) экшен бар приложения

--- app/src/main/java/com/example/as_lesson1/MainActivity.java
+++ app/src/main/java/com/example/as_lesson1/MainActivity.java
@@ -1,159 +1,217 @@
 package com.example.as_lesson1;
 
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText entryField;
+    TextView operationField;
+    TextView resultField;
+    String firstNumber;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Получаем поля ввода-вывода по идентификатору
-        TextView resultField = findViewById(R.id.result_field);
-        TextView operationField = findViewById(R.id.operation_field);
-        TextView entryField = findViewById(R.id.entry_field);
-
-        //Получаем кнопки по идентификатору
-        Button buttonOne = findViewById(R.id.button_one);
-        Button buttonTwo = findViewById(R.id.button_two);
-        Button buttonThree = findViewById(R.id.button_three);
-        Button buttonFour = findViewById(R.id.button_four);
-        Button buttonFive = findViewById(R.id.button_five);
-        Button buttonSix = findViewById(R.id.button_six);
-        Button buttonSeven = findViewById(R.id.button_seven);
-        Button buttonEight = findViewById(R.id.button_eight);
-        Button buttonNine = findViewById(R.id.button_nine);
-        Button buttonZero = findViewById(R.id.button_zero);
-        Button buttonPlus = findViewById(R.id.button_plus);
-        Button buttonMinus = findViewById(R.id.button_minus);
-        Button buttonMultiply = findViewById(R.id.button_multiply);
-        Button buttonDivision = findViewById(R.id.button_division);
-        Button buttonDot = findViewById(R.id.button_dot);
-        Button buttonEquals = findViewById(R.id.button_equals);
+        entryField = findViewById(R.id.entry_field);
+        operationField = findViewById(R.id.operation_field);
+        resultField = findViewById(R.id.result_field);
 
 
-        //Проставляем для каждой кнопки OnClickListener
+    }
 
-        buttonOne.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+    @SuppressLint("NonConstantResourceId")
+    public void clickNumber(View view) {
+        String number = entryField.getText().toString();
+        switch (view.getId()) {
+            case R.id.button_zero:
+                if (oneZeroFromStart(number) && number.length() == 1) {
+                    number = "0";
+                } else {
+                    number += "0";
+                }
+                break;
+            case R.id.button_one:
+                if (oneZeroFromStart(number) && number.length() == 1) {
+                    number = number.substring(1);
+                }
+                number += "1";
+                break;
+            case R.id.button_two:
+                if (oneZeroFromStart(number) && number.length() == 1) {
+                    number = number.substring(1);
+                }
+                number += "2";
+                break;
+            case R.id.button_three:
+                if (oneZeroFromStart(number) && number.length() == 1) {
+                    number = number.substring(1);
+                }
+                number += "3";
+                break;
+            case R.id.button_four:
+                if (oneZeroFromStart(number) && number.length() == 1) {
+                    number = number.substring(1);
+                }
+                number += "4";
+                break;
+            case R.id.button_five:
+                if (oneZeroFromStart(number) && number.length() == 1) {
+                    number = number.substring(1);
+                }
+                number += "5";
+                break;
+            case R.id.button_six:
+                if (oneZeroFromStart(number) && number.length() == 1) {
+                    number = number.substring(1);
+                }
+                number += "6";
+                break;
+            case R.id.button_seven:
+                if (oneZeroFromStart(number) && number.length() == 1) {
+                    number = number.substring(1);
+                }
+                number += "7";
+                break;
+            case R.id.button_eight:
+                if (oneZeroFromStart(number) && number.length() == 1) {
+                    number = number.substring(1);
+                }
+                number += "8";
+                break;
+            case R.id.button_nine:
+                if (oneZeroFromStart(number) && number.length() == 1) {
+                    number = number.substring(1);
+                }
+                number += "9";
+                break;
+            case R.id.button_dot:
+                if (oneZeroFromStart(number) && number.length() == 1) {
+                    number = number.substring(0);
+                }
+                if (oneDotInNum(number)) {
 
+                } else {
+                    number += ".";
+                }
+                break;
+            case R.id.button_clear:
+                number = "";
+                operationField.setText("");
+                if ((entryField.getText().toString()).equals("") &&
+                        (operationField.getText().toString()).equals(""))
+                    resultField.setText("");
+                break;
+            case R.id.button_backspace:
+                if (number.length() != 0) {
+                    number = number.substring(0, number.length() - 1);
+                } else if (number == null || number.length() == 0) {
+                    break;
+                }
+                break;
+
+        }
+        entryField.setText(number);
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    public void clickOperation(View view) {
+
+        String Operator = operationField.getText().toString();
+        firstNumber = entryField.getText().toString();
+
+        switch (view.getId()) {
+
+            case R.id.button_division:
+                Operator = "÷";
+                break;
+            case R.id.button_multiply:
+                Operator = "*";
+                break;
+            case R.id.button_plus:
+                Operator = "+";
+                break;
+            case R.id.button_minus:
+                Operator = "-";
+                break;
+
+        }
+        operationField.setText(Operator);
+        entryField.setText("");
+
+        if (firstNumber != null) {
+            resultField.setText(firstNumber);
+        } else if (firstNumber == null) {
+            resultField.setText("0");
+        }
+
+
+    }
+
+    @SuppressLint("SetTextI18n")
+    public void clickEquals(View view) {
+        String secondNumber = entryField.getText().toString();
+        Double result = 0.0;
+
+            switch (operationField.getText().toString()) {
+                case "÷":
+                    fieldsNotNull();
+                    result = Double.parseDouble(firstNumber) / Double.parseDouble(secondNumber);
+                    break;
+                case "*":
+                    fieldsNotNull();
+                    result = Double.parseDouble(firstNumber) * Double.parseDouble(secondNumber);
+                    break;
+                case "+":
+                    fieldsNotNull();
+                    result = Double.parseDouble(firstNumber) + Double.parseDouble(secondNumber);
+                    break;
+                case "-":
+                    fieldsNotNull();
+                    result = Double.parseDouble(firstNumber) - Double.parseDouble(secondNumber);
+                    break;
             }
-        });
+            resultField.setText(String.valueOf(result));
+            operationField.setText("=");
+            entryField.setText("");
 
-        buttonTwo.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+    }
 
-            }
-        });
+    public  void fieldsNotNull (){
+        if (entryField.getText().toString() == null || resultField.getText().toString() == null)
+        {
+            resultField.setText("0");
+            entryField.setText("");
+            operationField.setText("=");
+        }
+    }
+    public boolean oneDotInNum(String number) {
+        if (number.indexOf(".") == -1) {
+            return false;
+        } else {
+            return true;
+        }
 
-        buttonThree.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+    }
 
-            }
-        });
-
-        buttonFour.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
-
-        buttonFive.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
-
-        buttonSix.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
-
-        buttonSeven.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
-
-        buttonEight.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
-
-        buttonNine.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
-
-        buttonZero.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
-
-        buttonPlus.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
-
-        buttonMinus.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
-
-        buttonMultiply.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
-
-        buttonDivision.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
-
-        buttonDot.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
-
-        buttonEquals.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
-
-
+    public boolean oneZeroFromStart(String number) {
+        if (number.equals("")) {
+            return false;
+        } else if (number.charAt(0) == '0') {
+            return true;
+        } else {
+            return false;
+        }
     }
 
 }
